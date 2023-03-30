c5b62587d37b3b4370b02c876880d8c161e86d22
Rolandas021 <rolandas.tverijonas021@gmail.com>
2023-03-30 09:43:49 +0300

    New js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Vienos eilutes komentaras
+
+/*
+//multi
+//line
+//komentaras
+
+/*
+Multi
+line
+komentaras
+*/
+
+console.log(42);   //Komentaras salia
+console.log(41);
+console.log(40);
+
+//extra
+
+console.log(39);
+console.log(38);
+console.log(37);
